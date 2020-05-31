11b33eeb7e9a8bc79f685cbf5c65d502b8e0b1a1
lance-robb <lbrobb@byui.edu>
2020-05-31 13:47:28 -0600

    Commit

--- lesson6/js/toggleMenu.js
+++ lesson6/js/toggleMenu.js
@@ -1,4 +1,4 @@
-/* Option One -- Create function seperately */
+/*Option One -- Create function seperately 
 
 const hambutton = document.querySelector(".burger");
 hambutton.addEventListener("click", toggleMenu);
@@ -7,11 +7,14 @@ function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
 }
 
-/* Option Two -- function being created on the fly */
+Option Two -- function being created on the fly
 
-/*
 const hambutton = document.querySelector(".burger");
 const mainnav = document.querySelector(".navigation");
 
 hambutton.addEventListener("click", () => {mainnav.classList.toggle("responsive")});
 */
+
+function toggleMenu() {
+  document.getElementById("navigation").classList.toggle("hide");
+}
