98b16ebc8c083e990a71c643d3d17129d1f6250e
lance-robb <lbrobb@byui.edu>
2020-06-16 15:44:27 -0600

    Commit

--- lesson9/js/whatDay.js
+++ lesson9/js/whatDay.js
@@ -2,3 +2,14 @@
 
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
  document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', options);
+
+ /* Displaying on Friday only */
+
+let day = new Date();
+const banner = document.getElementById('banner');
+if(day.getDay() == 5) {
+    banner.style.display = "block";
+}
+else {
+    banner.style.display = "none";
+}
